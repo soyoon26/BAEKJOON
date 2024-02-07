c1f0619949966820000f52319051dd2abd9ad950
hangsangharr <122415737+soyoon26@users.noreply.github.com>
2024-02-07 12:24:21 +0900

    2024/02/07_자료구조

---
+++ S4 1620.나는야 포켓몬 마스터 이다솜.py
@@ -0,0 +1,15 @@
+import sys
+N,M=map(int,input().split())
+poke_dict={}
+for i in range(N):
+    poke=sys.stdin.readline().rstrip()
+    poke_dict[poke]=i+1
+    poke_dict[i+1]=poke
+for i in range(M):
+    test=sys.stdin.readline().rstrip()
+    if test.isdigit():
+        print(poke_dict[int(test)])
+    else:
+        print(poke_dict[test])
+
+#try except 썼으나 시간초과로 변경
