f7c70ac0c19a3d6f2f04cce3181f5b7e1f9b88e2
hangsangharr <122415737+soyoon26@users.noreply.github.com>
2024-02-16 11:13:55 +0900

    Delete S3 11659.구간 합 구하기 4.py

--- S3 11659.구간 합 구하기 4.py
+++
@@ -1,9 +0,0 @@
-import sys
-N,M=map(int,sys.stdin.readline().split())
-num=list(map(int,sys.stdin.readline().split()))
-sum=[num[0]]+[0]*N
-for i in range(1,N):
-    sum[i]=sum[i-1]+num[i]
-for i in range(M):
-    i,j=map(int,sys.stdin.readline().split())
-    print(sum[j-1]-sum[i-2])
