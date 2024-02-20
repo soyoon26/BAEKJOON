8adbf39d0f4160cb46f773dd2e70e5204df2b5ea
isang <122415737+soyoon26@users.noreply.github.com>
2024-02-20 10:52:03 +0900

    2024/02/20_스택

---
+++ S4 28278.스택 2.py
@@ -0,0 +1,25 @@
+import sys
+N=int(sys.stdin.readline().rstrip())
+stack=[]
+for i in range(N):
+    order=list(map(int,sys.stdin.readline().split()))
+    #list로 받지 않아도 됨
+    if order[0] == 1:
+        stack.append(order[1])
+    elif order[0] == 2:
+        if stack:
+            print(stack.pop())
+        else:
+            print(-1)
+    elif order[0] == 3:
+        print(len(stack))
+    elif order[0] == 4:
+        if stack:
+            print(0)
+        else:
+            print(1)
+    elif order[0] == 5:
+        if stack:
+            print(stack[-1])
+        else:
+            print(-1)
