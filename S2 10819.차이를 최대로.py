deb9951cfe7206a2bf990d0450a86140de5c6c29
isang <122415737+soyoon26@users.noreply.github.com>
2024-03-27 10:22:13 +0900

    2024/03/27_순열

---
+++ S2 10819.차이를 최대로.py
@@ -0,0 +1,14 @@
+import sys
+from itertools import permutations
+
+N = int(sys.stdin.readline().rstrip())
+num=map(int,sys.stdin.readline().split())
+per = list(permutations(num,N))
+answer=0
+for i in per:
+    ans=0
+    for j in range(N-1):
+        ans+=abs(i[j]-i[j+1])
+    if ans > answer:
+        answer=ans
+print(answer)
