64b9f0c937d283d372c78fe402bc2e1917113f56
isang <barcardi26@gmail.com>
2024-10-31 17:38:36 +0900

    2024/10/31_Tree

---
+++ S2 25511.값이 k인 트리 노드의 깊이.py
@@ -0,0 +1,25 @@
+import sys
+input = sys.stdin.readline
+sys.setrecursionlimit(10**6)
+n, k = map(int,input().split()) #정점 수, 정수
+#간선 정보
+graph=[[] for i in range(n)]
+
+for i in range(n-1):
+    u,v = map(int,sys.stdin.readline().split())
+    graph[u].append(v)
+    graph[v].append(u)
+nodes = list(map(int,input().split()))
+visited=[False]*n
+
+def dfs(start,cnt):
+    if nodes[start] == k:
+        print(cnt)
+        exit()
+    for j in graph[start]:
+        if not visited[j]:
+            visited[j] = True
+            dfs(j,cnt+1)
+            visited[j] = False
+visited[0] = True
+ans = dfs(0,0)
