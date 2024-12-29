f30a97bac37ecf610d8b6b299fd84a13ebd68a93
isang <barcardi26@gmail.com>
2024-12-29 20:23:13 +0900

    2024/12/29

---
+++ retake/Silver/S3 4375.1.py
@@ -0,0 +1,15 @@
+while True:
+    try:
+        n=int(input())
+        if n == 1:
+            print('1')
+            break
+        i='1'
+        while True:
+            if int(i)%n==0:
+                print(len(i))
+                break
+            else:
+                i+='1'
+    except:
+        break
